a18998e3c22054aa9d08ce3c33f0654ea546a9ad
Zakjanzi <zakjanzi97@gmail.com>
2023-04-27 11:55:34 +0300

    styling changes (symbol, name and % color)

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
@@ -5,10 +6,26 @@ import axios from 'axios';
 
 const API_KEY = process.env.API_KEY;
 
-export default function Home(props) {
-  const { result } = props;
+export default function Home() {
+  const [coins, setCoins] = useState([]);
 
-  const coins = result.Data;
+  useEffect(() => {
+    const fetchCoins = async () => {
+      const response = await axios.get(
+        `https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD&api_key=${API_KEY}`
+      );
+      const data = response.data;
+      setCoins(data.Data);
+    };
+
+    fetchCoins();
+
+    const intervalId = setInterval(fetchCoins, 60000); // fetch coins every minute
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
 
   return (
     <div className={styles.container}>
@@ -22,8 +39,9 @@ export default function Home(props) {
         <thead>
           <tr>
             <th>Symbol</th>
-            <th>24H Change</th>
+            <th>Name</th>
             <th>Price</th>
+            <th>24H Change</th>
             <th>Marketcap</th>
           </tr>
         </thead>
@@ -36,12 +54,16 @@ export default function Home(props) {
 
             return (
               <tr key={Name}>
-                <td>
-                  {/* <Image src={`https://www.cryptocompare.com${ImageUrl}`} width={20} height={20} alt={Name} /> */}
-                  {FullName}
+                <td className={styles.symbol}>
+                  <Image src={`https://www.cryptocompare.com${ImageUrl}`} width={40} height={40} alt={Name} />
                 </td>
-                <td>{CHANGEPCT24HOUR.toFixed(2)}%</td>
-                <td>{USD.PRICE.toFixed(2)}</td>
+               <td>
+               {FullName}
+               </td>
+                <td>{USD.PRICE.toLocaleString(undefined, {minimumFractionDigits: 2})}</td>
+                <td className={CHANGEPCT24HOUR >= 0 ? styles.green : styles.red}>
+                  {CHANGEPCT24HOUR.toFixed(2)}%
+                </td>
                 <td>{MKTCAP.toLocaleString()}</td>
               </tr>
             );
@@ -50,16 +72,4 @@ export default function Home(props) {
       </table>
     </div>
   );
-}
-
-export async function getStaticProps() {
-  const response = await axios.get(
-    `https://min-api.cryptocompare.com/data/top/mktcapfull?limit=34&tsym=USD&api_key=${API_KEY}`
-  );
-  const data = response.data;
-  return {
-    props: {
-      result: data,
-    },
-  };
 }
